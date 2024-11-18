2ef7a3150e36a89dd0ee6360b29f9f6452ac4d95
shini111 <126462128+shini111@users.noreply.github.com>
2024-11-18 17:36:50 +0000

    redo repository because of .git issues

---
+++ Engine/src/Engine.h
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <array>
+#include <Windows.h>
+
+#include "Animator.h"
+#include "GameLevel.h"
+#include "GameObjects.h"
+
+class Input {
+
+public:
+
+	std::vector <SHORT> ShootAction;
+	std::vector <SHORT> MoveRight;
+	std::vector <SHORT> MoveLeft;
+	std::vector <SHORT> MoveUp;
+	std::vector <SHORT> MoveDown;
+
+	void init();
+};
+
+
+class Game {
+public:
+
+	void start();
+
+
+private:
+	int prevTime = currentTime;
+	int currentTime = 0;
+};
+
+
+class GameWindow
+{
+public:
+	const char* windowName = "Xenon 2022";
+	int windowWidth = 640;
+	int windowHeight = 480;
+};
+
+namespace GameEngine {
+	class Engine
+	{
+	public:
+		float deltaTime = 0.0f;
+
+		void setLevel(GameLevel level);
+		GameLevel& getLevel();
+		void print(std::string printText);
+
+		void Update();
+		void Initialize(GameWindow windowSettings);
+	private:
+		GameLevel mainLevel;
+		GameWindow windowDisplay;
+		int prevTime = currentTime;
+		int currentTime = 0;
+
+	};
+
+}
